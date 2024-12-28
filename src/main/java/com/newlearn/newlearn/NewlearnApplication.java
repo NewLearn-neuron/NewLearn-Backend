a8dbf8d5b172afeab1f2541c0336e8daa96f7ae7
hyeseon <chunghye1998@gmail.com>
2024-12-29 00:43:01 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/newlearn/newlearn/NewlearnApplication.java
@@ -0,0 +1,13 @@
+package com.newlearn.newlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewlearnApplication.class, args);
+	}
+
+}
