c2b956be8c46148292f1467b15edf889f23b1c9e
hyeseon <chunghye1998@gmail.com>
2024-12-29 02:06:10 +0900

    feat: swagger, BaseApiResponse 사용 예제 API 추가

---
+++ src/main/java/com/newlearn/newlearn/TestController.java
@@ -0,0 +1,35 @@
+package com.newlearn.newlearn;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.newlearn.newlearn.global.exception.SuccessCode;
+import com.newlearn.newlearn.global.response.BaseApiResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@RestController
+public class TestController {
+
+	@Operation(summary = "테스트 API", description = "Swagger 테스트용 API입니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "테스트 성공")
+	})
+	@GetMapping("/test1")
+	public ResponseEntity<BaseApiResponse<String>> test() {
+		return BaseApiResponse.success(SuccessCode.TEST_SUCCESS);
+	}
+
+	@Operation(summary = "data 포함 테스트 API", description = "Swagger data 포함 테스트 API입니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "테스트 성공")
+	})
+	@GetMapping("/test2")
+	public ResponseEntity<BaseApiResponse<String>> testWithData() {
+		String data = "test";
+		return BaseApiResponse.success(SuccessCode.TEST_SUCCESS, data);
+	}
+}
--- src/main/java/com/newlearn/newlearn/global/exception/SuccessCode.java
+++ src/main/java/com/newlearn/newlearn/global/exception/SuccessCode.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SuccessCode implements BaseCode {
-	// ----- [News] ------ //
-	SHOW_NEWS_SUCCESS(HttpStatus.OK, "뉴스 상세 조회 성공");
+	// ----- [Test] ------ //
+	TEST_SUCCESS(HttpStatus.OK, "테스트 성공");
 
 	private final HttpStatus status;
 	private final String message;
